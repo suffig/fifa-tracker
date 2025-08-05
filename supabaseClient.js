5c21525efbe984fd090f17b2ac393634c2574ab7
suffig <philip-melchert@live.de>
2025-08-05 12:34:02 +0200

    session refresh

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,5 +2,13 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 export const supabase = createClient(
   'https://buduldeczjwnjvsckqat.supabase.co',
-  'sb_publishable_wcOHaKNEW9rQ3anrRNlEpA_r1_wGda3'
+  'sb_publishable_wcOHaKNEW9rQ3anrRNlEpA_r1_wGda3',
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true,
+      storageKey: 'supabase.auth.token'
+    }
+  }
 );
